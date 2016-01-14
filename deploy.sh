56b3f2ebd49a82e9048c01bf93bda5e9f338bff4
john <john.b.y.yang@gmail.com>
2016-01-14 17:55:07 +0800

    add expre in web

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@
 #      export http_proxy=http://127.0.0.1:8998
 #      export https_proxy=http://127.0.0.1:8999
 #      python2.7 /home/john/code/devpkg/python/google_appengine/appcfg.py --no_cookies --noisy --noauth_local_webserver update app.yaml
+export https_proxy=http://127.0.0.1:9001
 python2.7 /home/john/code/devpkg/python/google_appengine/appcfg.py  --noisy --noauth_local_webserver update app.yaml
